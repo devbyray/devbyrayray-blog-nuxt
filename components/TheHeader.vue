b4a3af3271f4b63f56814831884bae1b85805ebe
Dev By Ray <6241164+devbyray@users.noreply.github.com>
2022-11-03 14:33:12 +0100

    Update TheHeader.vue

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -12,12 +12,12 @@
 				</ul>
 			</nav>
 			<h1 class="dark:text-white text-black text-4xl font-bold mb-8">Dev By RayRay</h1>
-			<nuxt-img provider="cloudinary" loading="lazy" src="Dev_by_RayRay_-_logo" width="300" />
+			<nuxt-img provider="cloudinary" src="Dev_by_RayRay_-_logo" width="300" />
 			<em class="font-bold dark:text-white text-black text-2xl mb-16">{{ CONFIG?.sitedesc }}</em>
 		</header>
 		<header v-if="!home" class="page-max-md text-center page-header flex items-center flex-row mb-8">
 			<nuxt-link class="dark:text-white text-black" to="/">
-				<nuxt-img provider="cloudinary" loading="lazy" src="rayray.png" width="100" />
+				<nuxt-img provider="cloudinary" src="rayray.png" width="100" />
 				
 			</nuxt-link>
 			<nav class="w-full">
